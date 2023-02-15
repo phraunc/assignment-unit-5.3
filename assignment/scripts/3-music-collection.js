7c904d468f7428321cdb20f3601ea30595620e36
Anthony Cole <anthony.cole83@yahoo.com>
2023-02-15 13:39:16 -0600

    Created album object.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,14 @@
 console.log('***** Music Collection *****');
 
 let collection = [];
+
+let album = {
+    title: 'New Album',
+    artist:'Who',
+    yearPublished:'2012'
+} // end album.
+
+function addToCollection(title, artist, yearPublished){
+    console.log('In addToCollection function', title, artist, yearPublished);
+
+}// end fucntion 'addToCollection'.
