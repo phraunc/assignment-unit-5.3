2f85f44acba06985d3adaef6459c5730bbd01dd6
Anthony Cole <anthony.cole83@yahoo.com>
2023-02-16 11:55:11 -0600

    added 6 albums to the 'addToCollection' function.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,13 +2,21 @@ console.log('***** Music Collection *****');
 
 let collection = [];
 
-let album = {
-    title: 'New Album',
-    artist:'Who',
-    yearPublished:'2012'
-} // end album.
-
-function addToCollection(title, artist, yearPublished){
+function addToCollection(title, artist, yearPublished) {
     console.log('In addToCollection function', title, artist, yearPublished);
 
+    let album = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    }
+    collection.push(album);
+    return console.log('This is my Collection',album);
+
 }// end fucntion 'addToCollection'.
+addToCollection('Whats My Name Again', 'Blink-182', 1999);
+addToCollection('All The Small Things', 'Blink-182', 1998);
+addToCollection('Dammit', 'Blink-182', 1997);
+addToCollection('Let Her Cry', 'Hootie and the Blowfish', 1994);
+addToCollection('Only Want to be with You', 'Hootie and the Blowfish', 1994);
+addToCollection('Piano Man', 'Billy Joel', 1973);
